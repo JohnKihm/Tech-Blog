b2c50b8e63c572f1ea72108291db96b06fe33779
JohnKihm <john.a.kihm@gmail.com>
2024-07-08 02:43:36 -0500

    added script to handle create-post form submission

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -23,8 +23,11 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/create-post', withAuth, async (req, res) => {
     try {
+        const user_id = req.session.user_id;
+
         res.render('create-post', {
-            layout: 'dashboard'
+            layout: 'dashboard',
+            data: { user_id }
         });
     } catch (err) {
         res.status(500).json(err);
---
+++ public/js/createPost.js
@@ -0,0 +1,21 @@
+async function createPost(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+    const user_id = document.querySelector('#user_id').value;
+
+    if (title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content, user_id }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create project');
+        }
+    }
+}
